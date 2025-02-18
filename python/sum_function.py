579260c65b61ba2d467fea54226f59ba82838f83
MadhumithaS23 <madhumitha11300@gmail.com>
2025-02-18 02:54:24 -0800

    Your commit message

--- python/sum_function.py
+++ python/sum_function.py
@@ -1,5 +1,7 @@
-num 1=int(input("enter the number 1:")))
-num 2=int(input("enter the number 2:")))
+def sum_numbers(a,b):
+    return (a+b)
+num1=int(input("enter the number 1:"))
+num2=int(input("enter the number 2:"))
 
-sum=(num 1+num 2)
-print("the sum of two numbers is"+sum)
+
+print("sum:",sum_numbers(num1,num2))
